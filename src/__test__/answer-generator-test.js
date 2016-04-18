8925a0d18b237bb889a0ac69191d69bc6e6abe7a
Dale- <18717394542@163.com>
2016-04-19 00:25:43 +0800

    accomplish the class of AnswerGenerator

---
+++ src/__test__/answer-generator-test.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Dale on 4/18/16.
+ */
+import AnswerGenerator from '../answer-generator';
+
+describe('Class AnswerGenerator', () => {
+
+
+    let repeatCount;
+    let answerGenerator;
+
+    beforeEach(function () {
+
+        answerGenerator = new AnswerGenerator();
+
+        repeatCount = function (string, substring) {
+            let count = 0;
+            for (let i = 0; i < string.length; i++) {
+                if(substring === string[i]) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    });
+
+    describe('.getAnswer: ', () => {
+
+        it('use the method of getAnswer should return one string that length is four', () => {
+            let result = answerGenerator.getAnswer();
+            expect(result.length).toEqual(4);
+        });
+
+        it('use the method of getAnswer should return one string that all element is number', () => {
+            let result = answerGenerator.getAnswer();
+            var isAllTypeNumber = result.every(function (elem) {
+                return typeof(elem) === 'number';
+            });
+            expect(isAllTypeNumber).toBe(true);
+        });
+
+        it('use the method of getAnswer should return one string that element is not repeat', () => {
+            let result = answerGenerator.getAnswer();
+            let isAllTypeNumber = result.every(function (elem) {
+                return repeatCount(result, elem) === 1;
+            });
+            expect(isAllTypeNumber).toBe(true);
+        });
+
+    });
+});
