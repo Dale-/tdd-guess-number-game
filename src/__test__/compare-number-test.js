13b43ee2da7ad28ede9d3df3ae885f7060d5b2fc
Dale- <18717394542@163.com>
2016-04-18 22:07:40 +0800

    1234 compare to 1234 should return 4A0B

---
+++ src/__test__/compare-number-test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Dale on 4/17/16.
+ */
+import CompareNumber from '../compareNumber';
+
+describe('Class CompareNumber', () => {
+
+    describe('.compareNum: ', () => {
+
+        it('1234 compare to 1234 should return 4A0B', () => {
+            let result = CompareNumber.compareNum('1234', '1234');
+            expect(result).toEqual('4A0B');
+        });
+
+        //it('1234 compare to 5678 should return 0A0B', () => {
+        //    let result = CompareNumber.compareNum('1234', '5678');
+        //    expect(result).toEqual('0A0B');
+        //});
+    });
+});
+
---
+++ src/compareNumber.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Dale on 4/17/16.
+ */
+export default class CompareNumber {
+    static compareNum(answer, input) {
+        return '4A0B';
+    }
+}
